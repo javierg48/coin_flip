c37350f466781f0be234ae570718014035bc8665
android-47 <jx.garcia@outlook.com>
2021-03-25 21:37:16 -0700

    Removed using namespace std and updated README.md. Also, included array to hold the results of each coin flip.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# coin_flip
-Program that flips a coin x number of times.
+# Coin Flip
+
+Author: Javier Garcia Ramirez (android-47)
+Updated on: March 25, 2021
+
+### FEATURES
+* User can select x amount of times to flip a coin
+* Coin flip result is random for each flip
+* Displays the results of all x coin flips
+* Displays the total number of times the coin landed on heads and tails at the end
+
+## GAMEPLAY
+### 16 Coin Flips
+![16 Coin Flips](./images/16_flip.png "16 Coin Flips")
+### 1 Coin Flip
+![1 Coin Flip](./images/one_flip.png "1 Coin Flip")
+
+### DESCRIPTION
+This program allows the user to flip a coin x number of times. Each coin flip result displayed and is either heads or tails and is random. At the end of the program, the total number of times the coin landed on heads and tails is displayed.
+
+
+### STEPS ON HOW TO COMPILE AND RUN THE PROGRAM
+1. Compile the program using C++ 11 by entering the following into the terminal command line  
+
+```
+g++ -std=c++11 coin_flip.cpp
+```
+
+2. Run the program by running the executable file by entering the following line into the terminal command line:
+
+```
+./a.out
+```
+
+### KNOWN LIMITATIONS
+* No error handling 
--- coin_flip.cpp
+++ coin_flip.cpp
@@ -1,45 +1,46 @@
 // author: Javier Garcia Ramirez
 // create date: Saturday, January 11th, 2020
-// last modified: Sunday, January 12th, 2020
+// last modified: Thursday, March 25th, 2021
 // filename: coin_flip.cpp
 // description: flips a coin x times
 // runtime: n/a
 
 #include <iostream>
 #include <ctime>
-using namespace std;
 
-int number;
-int set;
-int headsCount = 0;
-int tailsCount = 0;
+
+void flip(int set, int *flipResults);
+
+
+int main() {   
+    int flipResults[] = { 0, 0 };
+    int set = 0;
+
+    std::cout << "\nEnter number of times to flip a coin: ";
+    std::cin >> set;
+
+    srand(time(NULL)); // This will randomize the number generated through the help of time.
+    flip(set, flipResults);
+
+    std::cout << "\nTotal amount of HEADS: " << flipResults[0] << std::endl;
+    std::cout << "Total amount of TAILS: " << flipResults[1] << std::endl << std::endl;
+    return 0;
+}
+
 
 // Selects either heads or tails based on the number of sets the user wants.
-int flip(){
-    for (int i=1; i <= set; i++){
-        number = rand() % 2 + 1;            // Randomizes number from 1-2.
-        if (number == 1){
-            cout << i << ". heads" << endl;
-            headsCount = headsCount + 1;    // Records the amount of times heads was a result.
-        }
-        else {
-            cout << i << ". tails" << endl;
-            tailsCount = tailsCount + 1;
+void flip(int set, int *flipResults) {
+    int rand_num = 0;
+    std::cout << std::endl;
+
+    for (unsigned i = 0; i < set; i++) {
+        rand_num = rand() % 2 + 1;            // Randomizes number from 1-2.
+        if (rand_num == 1) {
+            std::cout << i+1 << ". Coin flip result: Heads" << std::endl;
+            flipResults[0] += 1;
+        } else {
+            std::cout << i+1 << ". Coin flip result: Tails" << std::endl;
+            flipResults[1] += 1;
         }
     }
-    return number;
 }
-
-
-int main()
-{   
-    cout << "\nHow many times do you want to flip a coin? ";
-    cin >> set;
-    srand(time(0));         // This will randomize the number through the help of time.
-    flip();                 // Calls the flip function.   
-    cout << "Total amount of heads: " << headsCount << endl;
-    cout << "Total amount of tails: " << tailsCount << endl << endl;
-    return 0;
-
-}
-
