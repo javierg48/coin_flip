d19b385126f1f78ebd9d84c02219ec9f72460bbf
android-47 <jx.garcia@outlook.com>
2022-04-21 18:09:41 -0700

    Further modularized code and added track_tosses() function.

--- coin_flip.cpp
+++ coin_flip.cpp
@@ -1,6 +1,6 @@
 // author: Javier Garcia Ramirez
 // create date: Saturday, January 11th, 2020
-// last modified: Thursday, March 25th, 2021
+// last modified: Thursday, April 21st, 2022
 // filename: coin_flip.cpp
 // description: flips a coin x times
 // runtime: n/a
@@ -9,38 +9,44 @@
 #include <ctime>
 
 
-void flip(int set, int *flipResults);
+// outputs each flip result
+void track_tosses(int sets, int *heads, int *tails);
+
+// returns 1 for heads, 2 for tails
+int flip();
 
 
 int main() {   
-    int flipResults[] = { 0, 0 };
-    int set = 0;
+    srand(time(NULL)); // This will randomize the number generated through the help of time.
+    int flips = 0, heads_count = 0, tails_count = 0;
 
     std::cout << "\nEnter number of times to flip a coin: ";
-    std::cin >> set;
+    std::cin >> flips;
 
-    srand(time(NULL)); // This will randomize the number generated through the help of time.
-    flip(set, flipResults);
+	track_tosses(flips, &heads_count, &tails_count);
 
-    std::cout << "\nTotal amount of HEADS: " << flipResults[0] << std::endl;
-    std::cout << "Total amount of TAILS: " << flipResults[1] << std::endl << std::endl;
+    std::cout << "\nTotal amount of HEADS: " << heads_count << std::endl;
+    std::cout << "Total amount of TAILS: " << tails_count << std::endl << std::endl;
     return 0;
 }
 
 
-// Selects either heads or tails based on the number of sets the user wants.
-void flip(int set, int *flipResults) {
-    int rand_num = 0;
-    std::cout << std::endl;
-
-    for (unsigned i = 0; i < set; i++) {
-        rand_num = rand() % 2 + 1;            // Randomizes number from 1-2.
-        if (rand_num == 1) {
+void track_tosses(int sets, int *heads, int *tails){
+    for (unsigned i = 0; i < sets; i++) {
+		int toss = flip();
+        if (toss == 1) {
             std::cout << i+1 << ". Coin flip result: Heads" << std::endl;
-            flipResults[0] += 1;
+			(*heads)++;
         } else {
             std::cout << i+1 << ". Coin flip result: Tails" << std::endl;
-            flipResults[1] += 1;
+			(*tails)++;
         }
     }
+}
+
+
+// returns 1 for heads, 2 for tails
+int flip() {
+    int rand_num = rand() % 2 + 1;            // Randomizes number from 1-2.
+	return rand_num;
 }
